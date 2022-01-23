f71032902f75b2adef81502e68907ac301f27968
Mahsa Mojtabaee <mojtabaeemahsa@gmail.com>
2022-01-23 15:27:20 +0330

    addBank

--- src/FindBank.java
+++ src/FindBank.java
@@ -1,19 +1,31 @@
-public class FindBank {
-}
+public class FindBank {}
 
-class KDBank{
+class BankNode{
     private String name;
     private int X,Y;
-    private KDNode Banks;
-    void setName(String name){
+    private int depth = -1;
+    private BankNode right;
+    private BankNode left;
+    private BankNode Banks;
+    BankNode(String name, int X, int Y){
         this.name = name;
+        this.X = X;
+        this.Y = Y;
+    }
+    void setBanks(BankNode Banks){
+        this.Banks = Banks;
+    }
+    void setRight(BankNode right){
+        this.right = right;
+    }
+    void setLeft(BankNode left){
+        this.left = left;
+    }
+    void addDepth(){
+        this.depth += 1;
     }
     String getName(){
         return this.name;
-    }
-    void setCoordinates(int X, int Y){
-        this.X = X;
-        this.Y = Y;
     }
     int getX(){
         return this.X;
@@ -21,12 +33,52 @@ class KDBank{
     int getY(){
         return this.Y;
     }
-
+    int getDepth(){return this.depth;}
+    BankNode getRight(){ return this.right;}
+    BankNode getLeft(){ return this.left;}
 }
-class KDNode extends KDBank{
-    int x1,y1,x2,y2,x3,y3;
-}
+class Bank{
+    BankNode root;
+    void addBank(String name, int X, int Y){
+        BankNode node = new BankNode(name, X, Y);
+        if(this.root == null) {
+            root = node;
+            root.setBanks(null);
+            root.setRight(null);
+            root.setLeft(null);
+            root.addDepth();
+        }
+        else {
+            BankNode temp = this.root;
+            while (temp != null){
+                if (temp.getX() == node.getX() && temp.getY() == node.getY()){
+                    System.out.println("There is already a bank in this coordinates");
+                    return;
+                }
+                if(temp.getDepth()%2 == 0){
+                    if (temp.getX() > node.getX()){
+                        temp = temp.getLeft();
+                    }
+                    else {
+                        temp = temp.getRight();
+                    }
+                }
+                else {
+                    if (temp.getY() > node.getY()){
+                        temp = temp.getLeft();
+                    }
+                    else {
+                        temp = temp.getRight();
+                    }
+                }
+            }
+            temp = node;
+            temp.setRight(null);
+            temp.setLeft(null);
+            temp.addDepth();
+        }
+    }
 
-class KDTree{
-    KDNode root;
+
+//    int findLocal(){}
 }
